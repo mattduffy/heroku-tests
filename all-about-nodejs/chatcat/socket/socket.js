b95d90e97d5915fe98833d09104199fd0e01b161
Matthew Duffy <mattduffy@gmail.com>
2015-12-22 10:05:08 -0800

    Add changes made for lecture 46 exercises.

--- all-about-nodejs/chatcat/socket/socket.js
+++ all-about-nodejs/chatcat/socket/socket.js
@@ -19,12 +19,35 @@ module.exports = (io, rooms)=>{
         socket.username = data.user;
         socket.userPic = data.userPic;
         socket.join(data.room);
+        updateUserList(data.room,true);
       });
 
     socket.on('newMessage', function(data){
       socket.broadcast.to(data.room_number).emit('meesagefeed', JSON.stringify(data));
     });
+
+    function updateUserList(room,updateAll){
+      var getUsers = io.of('/messages').clients(room);
+      var userList = [];
+      for(var i in getUsers){
+        userList.push({
+          user:     getUsers[i].username,
+          userPic:  getUsers[i].userPic
+        });
+      }
+      userList = JSON.stringify(userList);
+      socket.to(room).emit('updateUserList', userList);
+      if(updateAll){
+        socket.broadcast.to(room).emit('updateUserList', userList);
+      }
+    }
+
+    socket.on('updateList', function(data){
+      console.log("debug updatList: ", data);
+      updateUserList(data.room, true);
+    });
   });
+
 
 
 };
